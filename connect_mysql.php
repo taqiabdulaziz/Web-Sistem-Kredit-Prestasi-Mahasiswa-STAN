067a94812cc4a47194710beb0f81dba6a9cf99e9
taqiabdulaziz <svx.annexiv@gmail.com>
2017-10-19 00:19:54 +0700

    Detail SKPM

--- connect_mysql.php
+++
@@ -1,5 +0,0 @@
-<?php
-//koneksi dan select database
-    $connection = mysqli_connect("localhost", "root", "", "u3124438_skpm-stan");
-
-  ?>
---
+++ view.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- for unminified version add this -->
+	<link rel="stylesheet" type="text/css" href="font/fonts.css" />
+	<!-- for minified version add this -->
+	<link rel="stylesheet" type="text/css" href="font/fonts.min.css" />
+  </
+  </head>
+  <body>
+    <div class="bg-header"></div>
+    <div class="header">
+    	<div class="container">
+    		<img src="img/logo.png"/>
+    		<h3>Politeknik Keuangan<br/>Negara STAN
+    		</h3>
+    		<h4>Sistem Kredit Prestasi Mahasiswa</h4>
+    	</div>
+    </div>
+    <div class="container sem-header">
+    <h4>Rencana Prestasi Mahasiswa Sem. Ganjil 2017</h4>
+
+    <a href="#"><div class=button>Tambah KRP</div></a>
+    </div>
+    <div class="table">
+    	<div class="container">
+			<table class="table-hover" style="width:100%">
+				<tr><th colspan="13" class="table-header">Tri Dharma Perguruan Tinggi</th></tr>
+  				<tr>
+    				<th>No</th>
+    				<th>KRPM</th>
+    				<th>Kode</th>
+    				<th>Nama</th>
+    				<th>Lingkup</th>
+    				<th>Tgl Mulai</th>
+    				<th>Tgl Selesai</th>
+    				<th>Nilai</th>
+    				<th>Keterangan</th>
+    				<th>Dokumen</th>
+    				<th>Hapus</th>
+    				<th>edit</th>
+    				<th>Status</th>
+  				</tr>
+  				<tr>
+    				<td>1</td>
+    				<td>7881</td>
+    				<td>LA1</td>
+    				<td>Capacity Building</td>
+    				<td>Lembaga</td>
+    				<td>02/04/2017</td>
+    				<td>05/04/2017</td>
+    				<td>25</td>
+    				<td>Peserta Kolter 1</td>
+    				<td>Sertifikat.jpg</td>
+    				<td>LA1</td>
+    				<td>LA1</td>
+    				<td>Diterima</td>
+  				</tr>
+  				<tr>
+    				<td>1</td>
+    				<td>7881</td>
+    				<td>LA1</td>
+    				<td>Capacity Building</td>
+    				<td>Lembaga</td>
+    				<td>02/04/2017</td>
+    				<td>05/04/2017</td>
+    				<td>25</td>
+    				<td>Peserta Kolter 1</td>
+    				<td>Sertifikat.jpg</td>
+    				<td>LA1</td>
+    				<td>LA1</td>
+    				<td>Diterima</td>
+  				</tr>
+  				<tr>
+    				<td>1</td>
+    				<td>7881</td>
+    				<td>LA1</td>
+    				<td>Capacity Building</td>
+    				<td>Lembaga</td>
+    				<td>02/04/2017</td>
+    				<td>05/04/2017</td>
+    				<td>25</td>
+    				<td>Peserta Kolter 1</td>
+    				<td>Sertifikat.jpg</td>
+    				<td>LA1</td>
+    				<td>LA1</td>
+    				<td>Diterima</td>
+  				</tr>
+  				<tr>
+    				<td>1</td>
+    				<td>7881</td>
+    				<td>LA1</td>
+    				<td>Capacity Building</td>
+    				<td>Lembaga</td>
+    				<td>02/04/2017</td>
+    				<td>05/04/2017</td>
+    				<td>25</td>
+    				<td>Peserta Kolter 1</td>
+    				<td>Sertifikat.jpg</td>
+    				<td>LA1</td>
+    				<td>LA1</td>
+    				<td>Diterima</td>
+  				</tr>
+			</table>    		
+    	</div>
+    </div>
+
+    <div class="table">
+    	<div class="container">
+			<table class="table-hover" style="width:100%">
+				<tr><th colspan="13" class="table-header">Organisasi</th></tr>
+  				<tr>
+    				<th>No</th>
+    				<th>KRPM</th>
+    				<th>Kode</th>
+    				<th>Nama</th>
+    				<th>Lingkup</th>
+    				<th>Tgl Mulai</th>
+    				<th>Tgl Selesai</th>
+    				<th>Nilai</th>
+    				<th>Keterangan</th>
+    				<th>Dokumen</th>
+    				<th>Hapus</th>
+    				<th>edit</th>
+    				<th>Status</th>
+  				</tr>
+  				<tr>
+    				<td>1</td>
+    				<td>7881</td>
+    				<td>LA1</td>
+    				<td>Capacity Building</td>
+    				<td>Lembaga</td>
+    				<td>02/04/2017</td>
+    				<td>05/04/2017</td>
+    				<td>25</td>
+    				<td>Peserta Kolter 1</td>
+    				<td>Sertifikat.jpg</td>
+    				<td>LA1</td>
+    				<td>LA1</td>
+    				<td>Diterima</td>
+  				</tr>
+  				<tr>
+    				<td>1</td>
+    				<td>7881</td>
+    				<td>LA1</td>
+    				<td>Capacity Building</td>
+    				<td>Lembaga</td>
+    				<td>02/04/2017</td>
+    				<td>05/04/2017</td>
+    				<td>25</td>
+    				<td>Peserta Kolter 1</td>
+    				<td>Sertifikat.jpg</td>
+    				<td>LA1</td>
+    				<td>LA1</td>
+    				<td>Diterima</td>
+  				</tr>
+  				<tr>
+    				<td>1</td>
+    				<td>7881</td>
+    				<td>LA1</td>
+    				<td>Capacity Building</td>
+    				<td>Lembaga</td>
+    				<td>02/04/2017</td>
+    				<td>05/04/2017</td>
+    				<td>25</td>
+    				<td>Peserta Kolter 1</td>
+    				<td>Sertifikat.jpg</td>
+    				<td>LA1</td>
+    				<td>LA1</td>
+    				<td>Diterima</td>
+  				</tr>
+  				<tr>
+    				<td>1</td>
+    				<td>7881</td>
+    				<td>LA1</td>
+    				<td>Capacity Building</td>
+    				<td>Lembaga</td>
+    				<td>02/04/2017</td>
+    				<td>05/04/2017</td>
+    				<td>25</td>
+    				<td>Peserta Kolter 1</td>
+    				<td>Sertifikat.jpg</td>
+    				<td>LA1</td>
+    				<td>LA1</td>
+    				<td>Diterima</td>
+  				</tr>
+			</table>    		
+    	</div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"></script>        
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+  		$( function() {
+    		$( "#datepicker" ).datepicker();
+  		} );    	
+    </script>
+  </body>
+</html>
